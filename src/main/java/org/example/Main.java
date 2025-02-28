a367b7cea5d79f2a269641385710661f088a91c0
manuel <moncadajuanmanuel25@gmail.com>
2025-02-27 20:16:19 -0500

    Revert "prueba"
    This reverts commit 0389cd6bd2f045a593a8237cab53d5263e5eb4b2.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,45 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("papa");
-        int piedra=1;
-        int papel=2;
-        int t
+        Scanner Scanner = new Scanner(System.in);
+
+        int cantidadinicial = 50000;
+        int cajero = 50000;
+        boolean cajerito = true;
+        while (cajerito) {
+            System.out.println("""
+                    ingrese una opcion
+                    1 consultar saldo
+                    2 ingresar dinero
+                    3 sacar dinero
+                    4 salir
+                    """);
+            int s = Scanner.nextInt();
+            switch (s) {
+                case 1 -> {
+                    System.out.println("la cantidad disponible es " + cajero);
+                }
+                case 2 -> {
+                    System.out.println("ingrese la cantidad a ingresar");
+                    int ingreso = Scanner.nextInt();
+                    cajero = ingreso + cantidadinicial;
+                }
+                case 3 -> {
+                    System.out.println("¿cuanto dinero desea retirar?");
+                    int retiro = Scanner.nextInt();
+                    if (cajero >= retiro) {
+                        cajero = cajero - retiro;
+
+                    } else System.out.println("no cuenta con el saldo suficiente");
+                }
+                case 4 -> {
+                    return;
+                }
+            }
+
+
+        }
+
 
 
     }
