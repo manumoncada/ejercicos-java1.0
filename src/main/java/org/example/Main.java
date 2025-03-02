b139d133498d8133b47f5c650e10d127f69b5abe
manuel <moncadajuanmanuel25@gmail.com>
2025-03-02 18:40:28 -0500

    actualizacion1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,60 @@ import java.util.Scanner;
 
 public class Main {
 
+    static Scanner Scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int dano = 0;
+        int defensa = 0;
+        int ataque = 0;
+        double neutro = 0.5;
+        int efectivo = 1;
+        int superefectivo=2;
+
+        while(ataque==0) {
+            System.out.println("introduzca el ataque de su pokemon");
+            ataque = Scanner.nextInt();
+            if (ataque > 100) {
+                System.out.println("el ataque es demasiado alto");
+                return;
+            } else if (ataque < 1) {
+                System.out.println("el ataque es demasiado bajo");
+                return;
+
+            }
+        }
+
+        System.out.println("""
+                selecione el tipo de su pokemon
+                1 electrico
+                2 agua
+                3 planta
+                4 fuego
+                """);
+        int tipo = Scanner.nextInt();
+        if (tipo==0) {}
+        switch(tipo){
+
+            case 1-> {
+                System.out.println("""
+                        seleccione el tipo de su rival
+                        1 electrico
+                        2 agua
+                        3 planta
+                        4 fuego""");
+                int rival= Scanner.nextInt();
+                if (rival==0);
+                switch(rival){
+                    case 1-> {
+                        System.out.println(defensa);
+                    }
+                }
+
+
+            }
+        }
+
+
+    }
 }
