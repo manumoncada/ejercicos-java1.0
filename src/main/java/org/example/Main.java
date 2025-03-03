a2d625e045203f3c0787ff93a7cad535012dbac6
manuel <moncadajuanmanuel25@gmail.com>
2025-03-03 11:58:22 -0500

    ajustes1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,23 +17,7 @@ public class Main {
         final int ATAQUE_EFECTIVO = 1;
         final int ATAQUE_SUPEREFECTIVO=2;
 
-        /*
-        while(ataque==0) {
-            System.out.println("introduzca el ataque de su pokemon");
-            ataque = Scanner.nextInt();
-            if (ataque > 100) {
-                System.out.println("el ataque es demasiado alto");
-                return;
-            } else if (ataque < 1) {
-                System.out.println("el ataque es demasiado bajo");
-                return;
 
-            }
-        }
-        */
-
-
-        //..............
 
         do{
             System.out.println("introduzca el ataque de su pokemon");
